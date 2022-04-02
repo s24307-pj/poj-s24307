bc9b07cf6223d5c33275bfa7035bb0a517101990
s24307-pj <92208975+s24307-pj@users.noreply.github.com>
2022-04-02 13:00:54 +0200

    Create TrojkatRownoramienny.java

---
+++ kolokwium3/TrojkatRownoramienny.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class TrojkatRownoramienny {
+
+    public <ramie> TrojkatRownoramienny(double podstawa, double ramie) {
+
+    }
+    public TrojkatRownoramienny(double podstawa) {
+
+    }
+
+    public double wysokosc(double v) {
+        return 0;
+    }
+
+    public double wysokosc() {
+    return 0;
+    }
+}
+
