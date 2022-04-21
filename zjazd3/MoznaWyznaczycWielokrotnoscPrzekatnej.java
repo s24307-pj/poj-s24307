88dc9fd0d6871533fa30d3aa9ae4220e3543b8f8
s24307-pj <92208975+s24307-pj@users.noreply.github.com>
2022-04-21 17:21:32 +0200

    Create MoznaWyznaczycWielokrotnoscPrzekatnej.java

---
+++ zjazd3/MoznaWyznaczycWielokrotnoscPrzekatnej.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public interface MoznaWyznaczycWielokrotnoscPrzekatnej extends MoznaWyznaczycPrzekatna {
+    double wielokrotnoscPrzekatnej(double v);
+
+    int wielokrotnoscPrzekatnej(int x);
+}
